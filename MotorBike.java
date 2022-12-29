0541f283ecc82c0ffa7e5087850e0782bd5fa764
Psm_01 <71458064+pp8817@users.noreply.github.com>
2022-12-29 20:20:14 +0900

    ㅏ

--- MotorBike.java
+++ MotorBike.java
@@ -1,15 +1,30 @@
 public class MotorBike {
   private int speed;
 
-  void setSpeed(int speed){
-    this.speed = speed;
-    System.out.println(speed);
-    System.out.println(this.speed);
+  MotorBike(){
+    this(5);
   }
 
-  int getSpeed(){
-    return this.speed;
+  MotorBike(int speed){
+    this.speed = speed;
+  }  
+  public int getSpeed(){
+    return speed;
   }
+  public void setSpeed(int speed){
+    if(speed > 0 )
+      this.speed=speed;
+  }
+
+  public void increaseSpeed(int howMuch){
+    setSpeed(this.speed +howMuch);
+  }
+  public void decreaseSpeed(int howMuch){
+    
+    setSpeed(this.speed-howMuch); 
+    
+  }
+  
     void start(){
       System.out.println("Bike Start");
     }
--- MotorBikeRunner.java
+++ MotorBikeRunner.java
@@ -1,12 +1,18 @@
 public class MotorBikeRunner {
   public static void main(String[] args) {
-    MotorBike ducati = new MotorBike();
-    MotorBike honda = new MotorBike();
+    MotorBike ducati = new MotorBike(100);
+    MotorBike honda = new MotorBike(200);
+    MotorBike somethingElse = new MotorBike();
+
+    System.out.println(ducati.getSpeed());
+    System.out.println(honda.getSpeed());
+    System.out.println(somethingElse.getSpeed());
+
     ducati.start();
     honda.start();
 
-    ducati.setSpeed(100);
-    // System.out.println(ducati.getSpeed());
+    ducati.setSpeed(-100);
+    System.out.println(ducati.getSpeed());
     // honda.setSpeed(80);
     // System.out.println(honda.getSpeed());
   }
