7de7babda3e38ddc8f122dab4ecdfb2013d8a012
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-09-29 14:29:40 +0900

    1. 객체와 테이블 매핑 2. 데이터베이스 스키마 자동 생성 3. 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,35 +16,22 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA"); //수정의 경우에는 persistence 할 필요 X,
 
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .getResultList(); //멤버 객체를 대상으로 쿼리, 객체가 대상
-//
-//            for (Member member : result) {
-//                System.out.println("member = " + member);
-//            }
-            //비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            //영속
-//            em.persist(member); //이때 db에 저장되는 것은 아님.
-            Member findMember1 = em.find(Member.class, 101L);//조회를 할 때 셀렉트 쿼리가 안나감. 이유? 1차 캐시에 저장이 됐기 때문에 pk 값으로 가져옴
-            Member findMember2 = em.find(Member.class, 101L); //이때는 쿼리가 안나감
+            em.persist(member);
 
-//            em.detach(member); 영속성 컨텍스트에서 지워버리는 
             tx.commit(); // 트랜잭션을 커밋하는 시점에서 영속성 컨텍스트에 있는 DB의 쿼리가 날라감
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close(); //중요, 엔티티 매니저가 결국 내부적으로 데이터베이스 커넥션을 물고 동작함. 사용 후 꼭 닫아줘야함.
         }
-
         emf.close();
-
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,18 +1,39 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-@Entity //jpa가 로딩될 때 인식을 함
-//@Table(name = "Member") //쿼리가 나갈 때 해당 테이블에 인서트하고 나감
+@Entity //jpa가 로딩될 때 인식을 함, JPA가 관리하는 객체가 되고 데이터베이스 테이블과 맵핑을 해서 사용함
+//@Table(name = "MBR") //쿼리가 나갈 때 해당 테이블에 인서트하고 나감
 public class Member {
-    @Id //pk가 무엇인지 알려줌
+    @Id //pk가 무엇인지 알려줌, pk 매핑
     private Long id;
+    @Column(name = "name") // 컬럼 이름, 안적어주면 필드 명으로 지정
+    private String username;
+    private Integer age;
 
-//    @Column(name = "name") // 컬럼 이름, 안적어주면 필드 명으로 지정
-    private String name;
+    @Enumerated(EnumType.STRING) //DB에는 enum 타입이 없음. 사용하기 위한 어노태이션
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate; //년 월만
+    private LocalDateTime testLocalDateTime;//년 월 일
+
+    @Lob
+    private String description;
+
+    /**
+     * Getter, Setter
+     */
+    public Member() {
+    }
 
     public Long getId() {
         return id;
@@ -22,11 +43,52 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
+
