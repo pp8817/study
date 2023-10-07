ea3e21127dd5cddeb467d6df36ddc3965375f7c6
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-07 23:54:28 +0900

    23.10.07 1.프록시 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,23 +22,22 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("hello");
-
             em.persist(member);
 
             em.flush(); //영속성 컨텍스트의 1차 캐시에 있는 정보를 db로 강제 전송
             em.clear(); //영속성 컨텍스트 초기화
 
-            //
-//            Member findMember = em.find(Member.class, member.getId());
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("ref.getClass() = " + refMember.getClass()); //Proxy
+            em.detach(refMember);
 
-            Member findMember = em.getReference(Member.class, member.getId()); //셀렉트 쿼리가 안나감
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+            System.out.println("refMember = " + refMember.getUsername()); //준영속 상태이기 때문에 데이터를 못가져옴 (오류 발생)
+//            em.close();
+
             tx.commit(); // 트랜잭션을 커밋하는 시점에서 영속성 컨텍스트에 있는 DB의 쿼리가 날라감
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close(); //중요, 엔티티 매니저가 결국 내부적으로 데이터베이스 커넥션을 물고 동작함. 사용 후 꼭 닫아줘야함.
         }
