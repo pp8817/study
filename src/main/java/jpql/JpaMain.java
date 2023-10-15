519092bbc5e9bbe6830debf4cc72a1f73475934c
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-15 16:35:46 +0900

    2023.10.15 1. 프로젝(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -17,24 +17,16 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            // 반환 타입이 명확할 때는 TypedQuery
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            em.flush();
+            em.clear();
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult = " + result.getUsername());
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
 
-            // 반환 타입이 명확하지 않을 때는 Query
-            Query query1 = em.createQuery("select m.username, m.age from Member m");
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
-            List<Member> resultList = query.getResultList(); //컬렉션 반환
-            Member result1 = query.getSingleResult(); //멤버 반환
-            System.out.println("result1 = " + result1);
-
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
 
             tx.commit();
         } catch (Exception e) {
