c24ff50e90bfc9a3ae7642e251501b9379ab0db9
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-28 14:40:08 +0900

    2023.10.28 1. 객체지향 쿼리 언어  - 프로젝션(SELECT) 공부

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -20,13 +20,27 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            List<Member> result = em.createQuery("select m from Member m ", Member.class)
+                    .getResultList(); // Entity들 반환, 반환된 엔티티들은 영속성 켄텍스트에서 관리가 될까?
+
+            Member findMember = result.get(0);
+            findMember.setAge(20); //Age가 20으로 변경된다면 영속성 켄텍스트에서 관리가 된다는 것.
+
+            //연관관계인 Team 엔티티 가져오기
+//            List<Team> resultTeam = em.createQuery("select m.team from Member m ", Team.class)
+//                    .getResultList();
+            //임베이드 타입 가져오기
+//            em.createQuery("select o.address from Order o", Address.class)
+//                    .getResultList();
+            //스칼라 타입 가져오기
+//            em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
                     .getResultList();
 
-            MemberDTO memberDTO = result.get(0);
-//            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-//            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
