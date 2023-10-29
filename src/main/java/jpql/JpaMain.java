dc8a2048036900ee01268423399fc725212c2b2c
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-29 16:47:07 +0900

    2023.10.29 1. 객체지향 쿼리 언어  - 경로 표현식  - FetchJoin

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -12,62 +13,43 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자 1");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자 2");
-            member1.setAge(10);
-            member1.setType(MemberType.ADMIN);
-
-            member1.setTeam(team);
-
+            member1.setUsername("회원 1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원 2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원 3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            String query3 = "select function('group_concat', m.username) From Member m";
-            List<String> result3 = em.createQuery(query3, String.class)
+            String query = "select t from Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
-            for (String s : result3) {
-                System.out.println("s = " + s);
+            System.out.println("result.size() = " + result.size());
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
-//
-//            String query1 =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금' " +
-//                            "     when m.age >=60 then '경로요금' " +
-//                            "     else '일반요금' " +
-//                            "end " +
-//                            "from Member m";
-//            List<String> result = em.createQuery(query1, String.class)
-//                    .getResultList();
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            }
-//
-//            String query2 = "select coalesce(m.username, '이름 없는 회원') as username " +
-//                    "from Member m";
-//            List<String> result2 = em.createQuery(query2, String.class)
-//                    .getResultList();
-//            for (String s : result2) {
-//                System.out.println("s = " + s);
-//            }
-            em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
 
             tx.commit();
         } catch (Exception e) {
