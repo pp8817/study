c1dfc6806383eb884ab1e63f455b35b76bcb1d19
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-02 12:46:36 +0900

    상품 엔티티에 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items", fetch = LAZY)
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addstock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStcok(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
