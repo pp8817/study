b55195cc649a04432c72fbbf021e3b82ce15b219
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-02 13:18:08 +0900

    주문, 주문상품 엔티티 개발 - 비즈니스 로직, 생성 메서드

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,6 +14,8 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders") //order는 예약어로 지정되어 있을수가 있기 때문에 관례로 orders로 해준다.
+@Getter
+@Setter
 public class Order {
     @Id
     @GeneratedValue
@@ -22,7 +27,7 @@ public class Order {
     private Member member; //연관관계의 주인
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<OrderItem> orderItems = new ArrayList<>();
+    private List<OrderItem> orderItems = new ArrayList<>(); //연관관계 편의 메서드로 인해 주문한 상품들이 모여있음.
 
     @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
@@ -48,4 +53,51 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+
+        /**
+         * 연관 관계 편의 메서드로 양방향 연관관계 설정
+         */
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem); //orderItems에 상품들을 더 넣어줌
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order; //order가 연관관계르 모두 걸면서 세팅이 되고 상태와 주문 시간 정보까지 다 세팅을 해서 반환된다.
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : this.orderItems) {
+            orderItem.cancel(); // getItem().addstock(count);
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
