ae00e1f892a1fc7f63aafaa08306f83baf2b6d39
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-05 22:57:40 +0900

    지연 로딩과 조회 성능 최적화를 공부 V1: 엔티티를 직접 노출   - 다양한 문제들이 발생 V2: 엔티티를 DTO로 변환   - V1의 문제들은 해결했지만 1 + N 문제 발생

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,77 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.repository.OrderRepository;
+import jpabook.jpashop.domain.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    /**
+     * 무한 루프에 빠지게 됨.
+     * 문제를 해결하려면 양방향 연관관계가 걸려있는 곳에 모두 @JsonIgnore을 해야함.
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        //ORDER 2개 조회
+        //N+1 or 1+ 회원 N + 배송 N 문제 -> 총 5번의 쿼리가 실행, 조회되는 ORDER의 수가 늘어날수록 쿼리는 기하급수적으로 증
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(result);
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); //Lazy 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+}
