29991982d5c3ab3f1cf4d29e4ec5ee76ad9e0a8a
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-06 11:42:50 +0900

    Result 클래스로 컬렉션을 감싸야 하는 이유에 대한 주석 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -13,11 +13,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Request, Response DTO 중첩 클래스를 만들 때 static을 붙이는 이유
- *   1. 메모리 누수의 원인을 예방할 수 있고, 클래스의 각 인스턴스당 더 적은 메모리를 사용하게 된다.
- *   2. static이 아닌 멤버 클래스는 바깥 인스턴스와 암묵적으로 연결이 되기 때문에 바깥 인스턴스 없이는 생성할 수 없다.
- *
- *   즉 멤버 클래스에서 바깥 인스턴스에 접근할 일이 없다면 무조건 static을 붙여 정적 멤버 클래스로 만들어주자!
+ Request, Response DTO 중첩 클래스를 만들 때 static을 붙이는 이유
+    1. 메모리 누수의 원인을 예방할 수 있고, 클래스의 각 인스턴스당 더 적은 메모리를 사용하게 된다.
+    2. static이 아닌 멤버 클래스는 바깥 인스턴스와 암묵적으로 연결이 되기 때문에 바깥 인스턴스 없이는 생성할 수 없다.
+
+    즉 멤버 클래스에서 바깥 인스턴스에 접근할 일이 없다면 무조건 static을 붙여 정적 멤버 클래스로 만들어주자!
  */
 @RestController
 @RequiredArgsConstructor
@@ -26,9 +26,10 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /**
-     * 엔티티를 직접 노출하게 되면 엔티티에 있는 정보들이 다 외부에 노출이 된다.
-     * Json 요청시 반환하고 싶지 않은 변수, 값들에 @JsonIgnore를 사용하면 된다.
-     * 하지만 API마다 요청이 다를 수 있기 때문에 이는 좋은 방식이 아니다.
+     *
+     엔티티를 직접 노출하게 되면 엔티티에 있는 정보들이 다 외부에 노출이 된다.
+     Json 요청시 반환하고 싶지 않은 변수, 값들에 @JsonIgnore를 사용하면 된다.
+     하지만 API마다 요청이 다를 수 있기 때문에 이는 좋은 방식이 아니다.
      */
     @GetMapping("/api/v1/members")
     public List<Member> membersV1() {
@@ -45,9 +46,20 @@ public class MemberApiController {
         return new Result(collect);
     }
 
+
+    /**
+     *
+     ※ Result 클래스로 컬렉션을 감싸는 이유는 Json 스펙에 맞춰 좀 더 유연하게 API를 반환하기 위함이다.
+
+     만약 감싸지 않을 경우에는 리스트 자체로 [ {Data1}, {Data2}, {Data3}, ,,,] 이런 식으로 반환된다. 만약 API가 요구하는 것이 Data 뿐만 아니라 Data의 갯수도 반환을 요청했을 경우
+     [ {Count}, {Data1}, {Data2}, {Data3} ,,] 이런 식으로 반환이 불가능하다. 리스트이기 때문에 Data와 Count객체가 동일해야한다.
+     따라서, 좀 더 유연한 Json 객체 반환을 위해 { "count" : 3, "Data" : [ {Data1}, {Data2}, {Data3} ] } 이런 식으로 반환해주어야한다.
+     그렇기 때문에 Result 클래스로 원하는 Json형태로 만들어 준 뒤 반환해주는 것이다.
+     */
     @Data
     @AllArgsConstructor
     static class Result<T> {
+        private int count; //Result 클래스로 컬렉션을 감싸준다면 Data의 갯수를 반환해달라는 요청을 유연하게 수행 가능하다.
         private T data;
     }
 
