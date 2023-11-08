b5f968044a9949eae1ca7a2e158076965d38e3ce
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-11-08 20:37:03 +0900

    V3: 엔티티를 조회해서 DTO로 변환(fetch join 사용O)      * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)      * <p>      * 단점: 페이징이 불가능함. V3.1에서 개선      * - 일대다에서 일(1)을 기준으로 페이징을 하는 것이 목적이다. 그런데 데이터는 다(N)를 기준으로 row가 생성된다.      * - Order를 기준으로 페이징 하고 싶은데, 다(N)인 OrderItem을 조인하면 OrderItem이 기준이 되어버린다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -11,11 +11,14 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -47,18 +50,63 @@ public class OrderApiController {
 
     /**
      * V2: 엔티티를 조회해서 DTO로 반환(fecth join 사용X)
-     트랜잭션 안에서 지연 로딩 필요
-
-     쿼리가 너무 많이 나간다. 1+N+N+N 번
+     * 트랜잭션 안에서 지연 로딩 필요
+     * <p>
+     * 쿼리가 너무 많이 나간다. 1+N+N+N 번
      */
     @GetMapping("/api/v2/orders")
     public Result ordersV2() {
         List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return new Result(collect);
     }
+
+
+    /**
+     * V3: 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+     * <p>
+     * 단점: 페이징이 불가능함. V3.1에서 개선
+     * - 일대다에서 일(1)을 기준으로 페이징을 하는 것이 목적이다. 그런데 데이터는 다(N)를 기준으로 row가 생성된다.
+     * - Order를 기준으로 페이징 하고 싶은데, 다(N)인 OrderItem을 조인하면 OrderItem이 기준이 되어버린다.
+     * <p>
+     * 컬렉션 fetch join은 사용하지 말자!
+     */
+    @GetMapping("/api/v3/orders")
+    public Result OrderV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+
+        return new Result(collect);
+    }
+
+    /**
+     * V3에서 페이징이 불가능하다는 문제가 있었다.
+     * 페이징 + 컬렉션 엔티티를 함께 조회하려면 어떻게 해야할까?
+     * 대부분의 페이징 + 컬렉션 엔티티 조회 문제는 아래 방법으로 해결할 수 있다.
+
+     먼저 ToOne 관계를 모두 페치 조인한다. ToOne 관계는 row수를 증가시키지 않으므로 페이징 쿼리에 영향을 주지 않는다.
+     컬렉션은 지연 로딩으로 조회한다.
+     지연 로딩 성능 최적화를 위해 hibernate.default_batch_fetch_size, @BatchSize 를 적용한다.
+     - hibernate.default_batch_fetch_size: 글로벌 설정
+     - @BatchSize: 개별 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public Result OrderV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                               @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return new Result(collect);
+    }
+
+
 
     @Data
     @AllArgsConstructor
@@ -71,8 +119,8 @@ public class OrderApiController {
     }
 
     /**
-     OrderItem 또한 엔티티이기 때문에 노출하면 안된다.
-     Address는 밸류 오브젝트이고 바뀔 일이 없기 때문에 상관없다.
+     * OrderItem 또한 엔티티이기 때문에 노출하면 안된다.
+     * Address는 밸류 오브젝트이고 바뀔 일이 없기 때문에 상관없다.
      */
     @Data
     static class OrderDto {
@@ -92,14 +140,14 @@ public class OrderApiController {
             this.address = order.getDelivery().getAddress();
             this.orderItems = order.getOrderItems().stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
     /**
-     OrderDto를 만들 때 OrderItem Entity의 데이터도 끌고 온다.
-     이때 Order만 Dto로 감싸주는 것이 아니라 OrderItem 또한 Dto로 감싸서
-     엔티티에 의존하는 것을 모두 없애야한다.
+     * OrderDto를 만들 때 OrderItem Entity의 데이터도 끌고 온다.
+     * 이때 Order만 Dto로 감싸주는 것이 아니라 OrderItem 또한 Dto로 감싸서
+     * 엔티티에 의존하는 것을 모두 없애야한다.
      */
     @Getter
     static class OrderItemDto {
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -76,6 +76,38 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    /**
+     fetch 조인으로 SQL이 1번만 실행된다.
+     distinct를 사용한 이유는 일대다 조인이 있으므로 데이터베이스 row가 증가한다.
+     그 결과 같인 order 엔티티의 조회 수도 증가하게 된다. JPA의 distinct는 SQL에 distinct를 추가하고,
+     더해서 같은 엔티티가 조회되면, 애플리케이션에서 중복을 걸러준다.
+     - 이 예에서 order가 컬렉션 fetch join 때문에 중복 조회 되는 것을 막아준다.
+
+     단점: 페이징 불가능
+     - 컬렉션 페치 조인을 사용하면 페이징이 불가능하다.
+     하이버네이트는 경고 로그를 남기면서 모든 데이터를 DB에서 읽어오고, 메모리에서 페이징 해버린다.(매우 위험)
+
+     컬렉션 페치 조인은 1개만 사용할 수 있ㄷ. 컬렉션 둘 이상에 페치 조인을 사용하면 안된다. 데이터가 부정합하게 조회된다.
+     */
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o " +
+                        "join fetch o.member m " +
+                        "join fetch o.delivery d " +
+                        "join fetch o.orderItems oi " +
+                        "join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o " +
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
 //    public List<OrderSimpleQueryDto> findOrderDtos() {
 //        /**
 //         원하는 값만 셀렉트해서 가져올 수 있음. 위보다 성능이 좋다.
