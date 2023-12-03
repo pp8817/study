f0ba14294a8ab650a6ab7e00d8f56c354fa889cb
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-12-03 22:16:03 +0900

    변수 문제 3개 풀이

---
+++ src/variable/ex/VarExqQuestion.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarExqQuestion {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+    }
+}
