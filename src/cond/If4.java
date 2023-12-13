da9430abec0e2e88aa47733d8cf82674ca0e4155
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-12-13 19:51:37 +0900

    조건문 if else if 문 적용

---
+++ src/cond/If4.java
@@ -0,0 +1,14 @@
+package cond;
+
+public class If4 {
+    public static void main(String[] args) {
+        int age = 14;
+        if (age <= 7) {
+            System.out.println("미취학");
+        } else if (age <= 13) {
+            System.out.println("초등학생");
+        } else if (age <= 16) {
+            System.out.println("중학생");
+        }
+    }
+}
