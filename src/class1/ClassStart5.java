f74cbb833e545451bdcde2c6146c8c1bc2c32cb8
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-12-22 16:39:02 +0900

    클래스에 배열을 적용해서 리팩토링

--- src/class1/ClassStart5.java
+++ src/class1/ClassStart5.java
@@ -1,6 +1,6 @@
 package class1;
 
-public class ClassStart3 {
+public class ClassStart5 {
     public static void main(String[] args) {
         Student student1;
         student1 = new Student();
@@ -10,10 +10,13 @@ public class ClassStart3 {
 
         Student student2 = new Student();
         student2.name = "학생2";
-        student2.age = 16;
+        student2.age = 16; 
         student2.grade = 80;
 
-        System.out.println("이름: " + student1.name + " 나이: " + student1.age + " 성적: " + student1.grade);
-        System.out.println("이름: " + student2.name + " 나이: " + student2.age + " 성적: " + student2.grade);
+        Student[] students = new Student[]{student1, student2};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("이름: " + students[i].name + " 나이: " + students[i].age + " 성적: " + students[i].grade);
+        }
     }
 }
