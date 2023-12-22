89977cc9465ee4a09d7b68bcf6c921c0eaf3c9f5
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-12-22 16:40:11 +0900

    향상된 for문 사용

--- src/class1/ClassStart5.java
+++ src/class1/ClassStart5.java
@@ -15,8 +15,12 @@ public class ClassStart5 {
 
         Student[] students = new Student[]{student1, student2};
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("이름: " + students[i].name + " 나이: " + students[i].age + " 성적: " + students[i].grade);
+//        for (int i = 0; i < students.length; i++) {
+//            System.out.println("이름: " + students[i].name + " 나이: " + students[i].age + " 성적: " + students[i].grade);
+//        }
+
+        for (Student s : students) {
+            System.out.println("이름: " + s.name + " 나이: " + s.age + " 성적: " + s.grade);
         }
     }
 }
