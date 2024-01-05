89befeb7f48e8b59ab54e4732b797f519740bb58
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-06 02:05:32 +0900

    static 공부

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,12 +1,22 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출");
         DecoData decoData = new DecoData();
         decoData.instanceCall();
+
+        //추가
+        //인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근
+        staticCall();
     }
 }
