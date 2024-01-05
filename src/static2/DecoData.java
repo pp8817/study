160abe476b0c3af3c9b28c92da2fcf064cb11f23
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-01-06 01:56:41 +0900

    instance와 static의 차이점 공부

---
+++ src/static2/DecoData.java
@@ -0,0 +1,29 @@
+package static2;
+
+public class DecoData {
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        //instanceValue++ ; 인스턴스 변수 접근, complie error
+        //instanceMethod(); 인스턴스 메서드 접근, complie error
+
+        staticValue++; //정적 변수 접근
+        staticMethod();
+    }
+
+    public void instanceCall() {
+        instanceValue++ ; // 인스턴스 변수 접근, complie error
+        instanceMethod(); // 인스턴스 메서드 접근, complie error
+
+        staticValue++; //정적 변수 접근
+        staticMethod();
+    }
+
+    private void instanceMethod() {
+        System.out.println("instanceValue=" + instanceValue);
+    }
+    private static void staticMethod() {
+        System.out.println("staticValue=" + staticValue);
+    }
+}
---
+++ src/static2/DecoDataMain.java
@@ -0,0 +1,12 @@
+package static2;
+
+public class DecoDataMain {
+    public static void main(String[] args) {
+        System.out.println("1. 정적 호출");
+        DecoData.staticCall();
+
+        System.out.println("2. 인스턴스 호출");
+        DecoData decoData = new DecoData();
+        decoData.instanceCall();
+    }
+}
