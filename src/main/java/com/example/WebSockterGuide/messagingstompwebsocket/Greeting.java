20785bacb17d77321a837d1e9ddcf2b695337d8d
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-03-30 16:00:12 +0900

    기초 엔티티와 Controller 구현

---
+++ src/main/java/com/example/WebSockterGuide/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,10 @@
+package com.example.WebSockterGuide.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/com/example/WebSockterGuide/messagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,11 @@
+package com.example.WebSockterGuide.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+public class HelloMessage {
+    private String name;
+}
