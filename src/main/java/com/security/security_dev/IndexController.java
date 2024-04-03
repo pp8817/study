7c112e2c1ae9a8099f551f3ce9d3178e379c8d37
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-04-03 22:44:58 +0900

    폼 인증 - formLogin() 공부

--- src/main/java/com/security/security_dev/IndexController.java
+++ src/main/java/com/security/security_dev/IndexController.java
@@ -1,5 +1,6 @@
 package com.security.security_dev;
 
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
     }
 }
