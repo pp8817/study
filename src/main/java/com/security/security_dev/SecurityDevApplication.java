55e4b299cbf7464aeea3f477d7ad5866cef0e41a
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-04-03 11:45:16 +0900

    프로젝트 생성과 의존성 설정

---
+++ src/main/java/com/security/security_dev/SecurityDevApplication.java
@@ -0,0 +1,13 @@
+package com.security.security_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/security_dev/SecurityDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.security_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
