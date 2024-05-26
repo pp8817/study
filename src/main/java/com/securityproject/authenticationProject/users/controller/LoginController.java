af27d6cc3ecd812f3dcfb479cc454e87bcb44d40
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-26 17:46:39 +0900

    GET 방식을 사용한 커스텀 Logout 구현

--- src/main/java/com/securityproject/authenticationProject/users/controller/LoginController.java
+++ src/main/java/com/securityproject/authenticationProject/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.securityproject.authenticationProject.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +19,16 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping("/logout") // 로그아웃을 GET 방식으로 구현, 그러나 POST 방식으로 하는게 더 안정적이다.
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
