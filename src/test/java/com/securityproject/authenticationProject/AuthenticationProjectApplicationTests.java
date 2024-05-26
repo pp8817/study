51e7b09753f68fc41c358e13f092698d90240cd6
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-26 13:36:35 +0900

    프로젝트 생성 및 기본 구성

---
+++ src/test/java/com/securityproject/authenticationProject/AuthenticationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityproject.authenticationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
