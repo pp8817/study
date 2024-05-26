9968a0aa637d74cf7df6039d5a7109b1345f2ab0
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-26 23:42:29 +0900

    커스텀 인증 성공, 실패 핸들러 구현

--- src/main/java/com/securityproject/authenticationProject/security/config/SecurityConfig.java
+++ src/main/java/com/securityproject/authenticationProject/security/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 
@@ -18,6 +20,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
+    private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,6 +33,8 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .authenticationDetailsSource(authenticationDetailsSource)
+                        .successHandler(successHandler)
+                        .failureHandler(failureHandler)
                         .permitAll()) // 커스텀 로그인 페이지 설정
                 .authenticationProvider(authenticationProvider);
         return http.build();
---
+++ src/main/java/com/securityproject/authenticationProject/security/handler/FormAuthenticationFailureHandler.java
@@ -0,0 +1,34 @@
+package com.securityproject.authenticationProject.security.handler;
+
+import com.securityproject.authenticationProject.security.exception.SecretException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FormAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String errorMessage = "Invalid Username or Password";
+
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "Invalid Username or Password";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "User not exists";
+        } else if (exception instanceof CredentialsExpiredException) {
+            errorMessage = "Expired password";
+        } else if (exception instanceof SecretException) {
+            errorMessage = "Invalid Secret key";
+        }
+        setDefaultFailureUrl("/login? error=true&exception="+errorMessage); //컨트롤러에서 해당 요청에 대한 처리를 해 주어야 한다.
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
